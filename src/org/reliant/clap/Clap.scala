318ec4a2be9b4c6db7d6b8ce9177e3ae42694ed6
Fred Dubois <fred.dubois@gmail.com>
2010-12-28 15:16:52 -0500

    Nothing much interesting

--- src/org/reliant/clap/Clap.scala
+++ src/org/reliant/clap/Clap.scala
@@ -3,42 +3,50 @@ package org.reliant.clap
 import scala.collection
 import collection.mutable.HashSet
 
-class ClapOption {
+class OptionSpec {
   private var short: Option[String] = None
   private var long: Option[String] = None
   private var meta: Option[String] = None
 
-  def setShort(_short: String) {
+  def setShort(_short: String): OptionSpec = {
     short = Some(_short)
+    this
   }
 
-  def setLong(_long: String) {
+  def setLong(_long: String): OptionSpec = {
     long = Some(_long)
+    this
   }
 
-  def setMeta(_meta: String) {
+  def setMeta(_meta: String): OptionSpec = {
     meta = Some(_meta)
+    this
   }
 
-  def isMatch(tentative: String) = short == tentative || long == tentative
+  def matches(target: String) = List(short, long) exists {_ exists (_ equals target)}
 }
 
 class Clap {
 
-  private val options = new HashSet[ClapOption]()
+  private val options = new HashSet[OptionSpec]()
 
-  def addOption(opt: ClapOption) {
+  def addOption(opt: OptionSpec) {
     options += opt
   }
 
   def parse(line: String) = {
-    def parseElem(elements: List[String]): List[ClapOption] = elements match {
+    def parseElem(elements: List[String]): List[OptionSpec] = elements match {
       case Nil => Nil
-      case head :: tail => options find {_.isMatch(head)} match {
-        case Some(option) => option + parseElem(tail)
+      case head :: tail => options find {_.matches(head)} match {
+        case Some(option) => option :: parseElem(tail)
         case _ => parseElem(tail)
       }
     }
-    parseElem(line split ' ')
+    parseElem(line split ' ' toList)
   }
+}
+
+object Main extends Application {
+  val opt = new OptionSpec setShort "d" setLong "debug"
+  println(opt matches "d")
 }
