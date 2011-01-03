72fb7c8ee061a77205c7450ebaf38c24c347ad6d
Fred Dubois <fred.dubois@gmail.com>
2011-01-02 21:51:33 -0500

    Playing around with traits
    Traits are fun

--- src/main/scala/org/reliant/clap/Clap.scala
+++ src/main/scala/org/reliant/clap/Clap.scala
@@ -26,9 +26,47 @@ class OptionSpec (val name: String) {
   def matches(target: String) = List(short, long) exists {_ exists (_ equals target)}
 }
 
+abstract class OptionSpecification {
+  def getDefinedOptions(): List[String] = Nil
+}
+
+abstract class PosixOption(val name: String) extends OptionSpecification
+
+trait ShortOpt extends OptionSpecification {
+  val short: String
+
+  abstract override def getDefinedOptions() = short :: super.getDefinedOptions
+}
+
+trait LongOpt extends OptionSpecification {
+  val long: String
+
+  abstract override def getDefinedOptions() = long :: super.getDefinedOptions
+}
+
 class ClapException extends Exception
 
 class BadOptionException extends ClapException
+
+class NoOptionValueException extends ClapException
+
+object OptionHelpers {
+
+  def isLong(opt: String) = opt startsWith "--"
+
+  def isShort(opt: String) = !isLong(opt) && (opt startsWith "-")
+
+  def hasValue(opt: String) =
+    isLong(opt) && ((opt count (_ == '=')) == 1) && !(opt endsWith "=")
+
+  def getValue(opt: String) =
+    if (hasValue(opt)) {
+      val _ :: value :: Nil = opt split '=' toList;
+      Some(value)
+    }
+    else
+      None
+}
 
 class Clap {
 
@@ -44,19 +82,11 @@ class Clap {
   private def parseLongOpt(elem: String): (String, Option[String]) =
     elem count (_ == '=') match {
       case 0 => (elem, None)
-      case 1 => {
-        val name :: value :: Nil = elem split '=' toList
-        (name, Some(value))
-      }
+      case 1 => (elem, OptionHelpers.getValue(elem))
       case _ => throw new BadOptionException
     }
 
   def parse(line: String) = {
-    def parseElem(elements: List[String]): (Options, Args) = elements match {
-      case head :: tail => head match {
-
-      }
-    }
     def parseElems(elements: List[String], opts: Options, args: Args): (Options, Args) = elements match {
       case Nil => (opts, args)
       case head :: tail => options find {_.matches(head)} match {
@@ -71,4 +101,11 @@ class Clap {
 object Main extends Application {
   val opt = new OptionSpec("debug") setShort "d" setLong "debug"
   println(opt matches "d")
+
+  val test = new PosixOption("port") with ShortOpt with LongOpt {
+    val short = "p"
+    val long = "port"
+  }
+
+  test getDefinedOptions() map println
 }
